a2a86f6028348f16e08749f54181753d2bbf74e5
rajeshthakur30 <rajeshthakur0905@gmail.com>
2025-12-16 23:45:56 +0530

    StringRotationOfEachOher

---
+++ GfgLeetcode/16.12StringRotationOfEachOther.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool areRotations(string s1, string s2) {
+        // Step 1: Length check
+        if (s1.length() != s2.length())
+            return false;
+
+        // Step 2: Concatenate s1 with itself
+        string temp = s1 + s1;
+
+        // Step 3: Check if s2 is a substring of temp
+        if (temp.find(s2) != string::npos)
+            return true;
+
+        return false;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s1 = "abcd";
+    string s2 = "cdab";
+
+    if (obj.areRotations(s1, s2))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
