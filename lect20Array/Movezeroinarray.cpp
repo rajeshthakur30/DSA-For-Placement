921472482498cb67b01dd728faaf85b1e1ac4dcc
rajeshthakur30 <rajeshthakur0905@gmail.com>
2026-02-02 23:44:14 +0530

    2Feb26

--- lect20Array/Movezeroinarray.cpp
+++ lect20Array/Movezeroinarray.cpp
@@ -2,11 +2,12 @@
 #include<vector>
 using namespace std;
 vector<int>moveZeroes(vector<int> &nums){
-    int nonZeroes=0;
+    // int nonZeroes=0;
+    int i=0;
     for(int j=0;j<nums.size();j++){
        if(nums[j]!=0){
-           swap(nums[j],nums[nonZeroes]);
-           nonZeroes++;
+           swap(nums[j],nums[i]);
+           i++;
         }
     }
       return nums;
